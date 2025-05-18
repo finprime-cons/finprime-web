89fccad88ff28186693fe7eb44572f1e5e096b17
Adwaith A <adwaithchikku14@gmail.com>
2025-05-18 11:45:43 +0530

    swiper addd

--- src/project-1-main/components/Text/Text1.jsx
+++ src/project-1-main/components/Text/Text1.jsx
@@ -37,10 +37,10 @@ const Text1 = () => {
 
         ref={fadeElementRef}
         className={`my-[35px] sm:my-20 font-khula xl:px-12 lg:px-10 md:px-8 px-6 pt-4  ${hasFadedIn ? 'animate-fadeinbottom' : ''}`}>
-        <h1 className='text-[34px] md:text-6xl leading-9  lg:text-6xl xl:text-9xl font-bold text-left'>
+        <h1 className='text-[34px] md:text-6xl leading-9  lg:text-6xl xl:text-8xl font-bold text-left'>
           Preparing Your Success,
         </h1>
-        <h4 className='mt-14 mb-14 sm:mb-24  lg:mb-44 text-xs sm:text-xl  lg:text-3xl font-light text-left'>
+        <h4 className='mt-6 sm:mt-14 mb-14 sm:mb-24  lg:mb-44 text-xs sm:text-xl  lg:text-3xl font-light text-left'>
           We Provide Truly Prominent Accounting Services
         </h4>
         <Certificates />
