1a0630ce739f6f6d64d9e1293f493dc5a0e01dce
Adwaith A <adwaithchikku14@gmail.com>
2025-05-19 11:09:34 +0530

    added the navigation of service in banner slider

--- src/project-1-main/components/Bannerslider/Bannerslider.jsx
+++ src/project-1-main/components/Bannerslider/Bannerslider.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FaFacebookF, FaXTwitter,FaYoutube} from "react-icons/fa6";
+import { FaFacebookF, FaXTwitter, FaYoutube } from "react-icons/fa6";
 import { RiLinkedinFill } from "react-icons/ri";
 import { FaInstagramSquare } from "react-icons/fa";
 // import { IoIosClose } from 'react-icons/io';
@@ -58,44 +58,44 @@ const Bannerslider = () => {
 
 
 
-//   const [isSpeakExpert, setIsSpeakExpert] = useState(false);
+  //   const [isSpeakExpert, setIsSpeakExpert] = useState(false);
 
-//   const toggleSpeakExpert = () => {
-//     setIsSpeakExpert(!isSpeakExpert);
-// };
+  //   const toggleSpeakExpert = () => {
+  //     setIsSpeakExpert(!isSpeakExpert);
+  // };
 
-//   useEffect(() => {
-//     if (isSpeakExpert) {
-//         document.body.style.overflow = 'hidden';  
-//     } else {
-//         document.body.style.overflow = '';  
-//     }
-//     return () => {
-//         document.body.style.overflow = '';  
-//     };
-// }, [isSpeakExpert]);
-
-
-// const [selectedService, setSelectedService] = useState("");  // State for selected service
-// const [selectedSubtitle, setSelectedSubtitle] = useState(""); // State for selected subtitle
-// const [selectedIndexOpen, setSelectedIndexOpen] = useState(null); // Index of the selected service
-
-// const handleServiceChange = (e) => {
-//   const serviceTitle = e.target.value;
-//   setSelectedService(serviceTitle);
-
-//   // Find the index of the selected service from the Services array
-//   const selectedServiceIndex = Services.findIndex(service => service.title === serviceTitle);
-//   setSelectedIndexOpen(selectedServiceIndex);
-//   setSelectedSubtitle("");  // Reset subtitle when service changes
-// };
-
-// const handleSubtitleChange = (e) => {
-//   setSelectedSubtitle(e.target.value);
-// };
+  //   useEffect(() => {
+  //     if (isSpeakExpert) {
+  //         document.body.style.overflow = 'hidden';  
+  //     } else {
+  //         document.body.style.overflow = '';  
+  //     }
+  //     return () => {
+  //         document.body.style.overflow = '';  
+  //     };
+  // }, [isSpeakExpert]);
 
 
- 
+  // const [selectedService, setSelectedService] = useState("");  // State for selected service
+  // const [selectedSubtitle, setSelectedSubtitle] = useState(""); // State for selected subtitle
+  // const [selectedIndexOpen, setSelectedIndexOpen] = useState(null); // Index of the selected service
+
+  // const handleServiceChange = (e) => {
+  //   const serviceTitle = e.target.value;
+  //   setSelectedService(serviceTitle);
+
+  //   // Find the index of the selected service from the Services array
+  //   const selectedServiceIndex = Services.findIndex(service => service.title === serviceTitle);
+  //   setSelectedIndexOpen(selectedServiceIndex);
+  //   setSelectedSubtitle("");  // Reset subtitle when service changes
+  // };
+
+  // const handleSubtitleChange = (e) => {
+  //   setSelectedSubtitle(e.target.value);
+  // };
+
+
+
   return (
     <div className="relative w-full h-screen overflow-hidden">
       {/* Background Video */}
@@ -109,61 +109,65 @@ const Bannerslider = () => {
       >
         <source src={videos[currentSlide]} type="video/mp4" />
       </video>
-      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40 " /> 
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40 " />
 
       {/* Banner Content */}
       <div className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full text-white ">
         <div className="absolute left-0 justify-center pl-6 mt-52 lg:pl-10 xl:pl-12 md:pl-8">
-        <h3
-              className='sm:ml-20 w-50%  text-left text-[26px] leading-7 sm:leading-none font-khula lg:text-4xl  xl:text-5xl text-white 
+          <h3
+            className='sm:ml-20 w-50%  text-left text-[26px] leading-7 sm:leading-none font-khula lg:text-4xl  xl:text-5xl text-white 
               font-semibold mb-3 
               z-20 duration-100  pr-6 '>
-                
-                {CurrentTitle.headtitle}
-              </h3>
-              <div className='z-20 flex sm:ml-20'>
-              <Link
-                to={`/${CurrentTitle.title}/${CurrentSubtitles[currentSubtitleIndex]?.subtitle}/${CurrentSubtitles[currentSubtitleIndex]?.subtitle}`}
-                className="font-raleway cursor-pointer leading-6 text-left font-medium  text-lg sm:text-lg lg:text-[24px] tracking-[2px] text-white"
-              >
-                <span className="relative inline-block pr-10">
-                  {/* Loader with the gradient line */}
-                  <span className="hidden sm:block absolute inset-0 w-full h-[1px] bg-gradient-to-r from-brandBlue to-cyan-500 top-1/2 -translate-y-1/2 "></span>
 
-                  {/* Text with higher z-index to be above the loader */}
-                  <span className="relative z-20 loader ">
-                    {CurrentSubtitles[currentSubtitleIndex]?.headsubtitle}
-                  </span>
+            {CurrentTitle.headtitle}
+          </h3>
+          <div className='z-20 flex sm:ml-20'>
+            <Link
+              state={{
+                service_id: CurrentTitle.id,
+                subtitles_id: CurrentSubtitles[currentSubtitleIndex]?.subid
+              }}
+              to={`/services/${CurrentTitle.title.replace(/\s+/g, '-')}/${CurrentSubtitles[currentSubtitleIndex]?.keyword.replace(/\s+/g, '-')}`}
+              className="font-raleway cursor-pointer leading-6 text-left font-medium  text-lg sm:text-lg lg:text-[24px] tracking-[2px] text-white"
+            >
+              <span className="relative inline-block pr-10">
+                {/* Loader with the gradient line */}
+                <span className="hidden sm:block absolute inset-0 w-full h-[1px] bg-gradient-to-r from-brandBlue to-cyan-500 top-1/2 -translate-y-1/2 "></span>
+
+                {/* Text with higher z-index to be above the loader */}
+                <span className="relative z-20 loader ">
+                  {CurrentSubtitles[currentSubtitleIndex]?.headsubtitle}
                 </span>
-              </Link>
+              </span>
+            </Link>
 
 
           </div>
           <div>
-          <div  className='flex mt-36'>
-         <div >
-         <button
-            onClick={handlePrevSlide}
-            className="px-2 py-2 mr-2 text-4xl text-black transition-all duration-300 ease-in-out bg-white rounded-full sm:px-3 sm:py-3 sm:mr-2 hover:bg-gradient-to-r hover:from-brandBlue hover:to-cyan-500 hover:bg-opacity-40 hover:text-white"
-          >
-            <HiOutlineArrowSmallLeft className="text-lg" />
-          </button>
-          <button
-            onClick={handleNextSlide}
-            className="px-2 py-2 mr-2 text-4xl text-black transition-all duration-300 ease-in-out bg-white rounded-full sm:px-3 sm:py-3 sm:mr-2 hover:bg-gradient-to-r hover:from-brandBlue hover:to-cyan-500 hover:bg-opacity-40 hover:text-white"
-          >
-            <HiOutlineArrowSmallRight className="text-lg" />
-          </button>
-         </div>
-          <Link
-          to={`/form2/$${CurrentTitle.id}/${CurrentSubtitles[currentSubtitleIndex]?.subid}`}
-          // onClick={toggleSpeakExpert}
-            className='text-center sm:text-left   cursor-pointer text-[15px] bg-brandBlue sm:text-[16px] sm:tracking-[2px] tracking-[1px] text-white border border-cyan-700 rounded-[5px]
+            <div className='flex mt-36'>
+              <div >
+                <button
+                  onClick={handlePrevSlide}
+                  className="px-2 py-2 mr-2 text-4xl text-black transition-all duration-300 ease-in-out bg-white rounded-full sm:px-3 sm:py-3 sm:mr-2 hover:bg-gradient-to-r hover:from-brandBlue hover:to-cyan-500 hover:bg-opacity-40 hover:text-white"
+                >
+                  <HiOutlineArrowSmallLeft className="text-lg" />
+                </button>
+                <button
+                  onClick={handleNextSlide}
+                  className="px-2 py-2 mr-2 text-4xl text-black transition-all duration-300 ease-in-out bg-white rounded-full sm:px-3 sm:py-3 sm:mr-2 hover:bg-gradient-to-r hover:from-brandBlue hover:to-cyan-500 hover:bg-opacity-40 hover:text-white"
+                >
+                  <HiOutlineArrowSmallRight className="text-lg" />
+                </button>
+              </div>
+              <Link
+                to={`/form2/$${CurrentTitle.id}/${CurrentSubtitles[currentSubtitleIndex]?.subid}`}
+                // onClick={toggleSpeakExpert}
+                className='text-center sm:text-left   cursor-pointer text-[15px] bg-brandBlue sm:text-[16px] sm:tracking-[2px] tracking-[1px] text-white border border-cyan-700 rounded-[5px]
              transition-all duration-300 ease-out hover:bg-gradient-to-r hover:from-brandBlue hover:to-cyan-500 
            mb-10 sm:mb-20 sm:py-3 px-3 py-2 sm:px-5 sm:ml-2'>
-              <p>Make an Appointment</p>
-            </Link>
-            {/* {isSpeakExpert && (
+                <p>Make an Appointment</p>
+              </Link>
+              {/* {isSpeakExpert && (
                                     <div
                                         className="fixed inset-0 w-screen h-[100vh] bg-black
                                          bg-opacity-60 z-50 flex justify-center items-center"
@@ -260,16 +264,16 @@ const Bannerslider = () => {
                                         </div>
                                     </div>
                                 )} */}
-          </div>
+            </div>
           </div>
         </div>
 
         {/* Icons on the right */}
         <div className="absolute flex-col justify-center hidden gap-4 text-3xl md:block sm:right-8 bottom-52 sm:flex sm:mb-3 lg:mb-0 pb-72 sm:pb-0">
-        <div className="flex space-x-2 bg-white bg-opacity-55 rounded-[5px] border border-opacity-20  p-1 xl:p-3 -mb-1 ">
+          <div className="flex space-x-2 bg-white bg-opacity-55 rounded-[5px] border border-opacity-20  p-1 xl:p-3 -mb-1 ">
             <button className="   pl-4 pr-4 rounded-sm   shadow-black  text-[16px] tracking-[1px]
             transition-all duration-300 ease-out bg-gradient-to-r from-brandBlue to-cyan-500 text-white">Follow</button>
-             <a href="https://www.facebook.com/finprimeconsulting" aria-label="Facebook" className="p-2 text-black transition-all duration-300 ease-out bg-white rounded-full shadow-black hover:bg-gradient-to-r hover:border-opacity-10 hover:from-brandBlue hover:to-cyan-500 hover:text-white">
+            <a href="https://www.facebook.com/finprimeconsulting" aria-label="Facebook" className="p-2 text-black transition-all duration-300 ease-out bg-white rounded-full shadow-black hover:bg-gradient-to-r hover:border-opacity-10 hover:from-brandBlue hover:to-cyan-500 hover:text-white">
               <FaFacebookF size={20} />
             </a>
             <a href="https://x.com/FinPrimeConsult" aria-label="Twitter" className="p-2 text-black transition-all duration-300 ease-out bg-white rounded-full shadow-black hover:bg-gradient-to-r hover:from-brandBlue hover:border-opacity-10 hover:to-cyan-500 hover:text-white ">
@@ -279,12 +283,12 @@ const Bannerslider = () => {
               <RiLinkedinFill size={20} />
             </a>
             <a href="https://www.instagram.com/finprimeconsulting/" aria-label="WhatsApp" className="p-2 text-black transition-all duration-300 ease-out bg-white rounded-full shadow-black hover:bg-gradient-to-r hover:from-brandBlue hover:to-cyan-500 hover:text-white hover:border-opacity-10">
-              <FaInstagramSquare  size={20} />
+              <FaInstagramSquare size={20} />
             </a>
             <a href="https://www.youtube.com/@FinPrimeConsulting" aria-label="WhatsApp" className="p-2 text-black transition-all duration-300 ease-out bg-white rounded-full shadow-black hover:bg-gradient-to-r hover:from-brandBlue hover:to-cyan-500 hover:text-white hover:border-opacity-10">
-              <FaYoutube  size={20} />
+              <FaYoutube size={20} />
             </a>
-           
+
           </div >
         </div>
 
@@ -300,30 +304,30 @@ const Bannerslider = () => {
         </div>
 
 
-        
+
 
         {/* Paragraph at the bottom */}
         <div className="absolute bottom-0 flex flex-col w-full text-center sm:pl-0 sm:pr-0 ">
-       <div className={`flex flex-wrap justify-between font-raleway text-[10px] md:text-[14px] tracking-[2px]   `}>
-          <button className="relative flex-1 px-3 py-2 overflow-hidden text-white uppercase transition-all duration-300 ease-out bg-black lg:py-6 group hover:bg-gradient-to-r hover:from-brandBlue hover:to-cyan-500">
-            <span className="absolute right-0 w-10 h-32 -mt-12 transition-all duration-1000 
+          <div className={`flex flex-wrap justify-between font-raleway text-[10px] md:text-[14px] tracking-[2px]   `}>
+            <button className="relative flex-1 px-3 py-2 overflow-hidden text-white uppercase transition-all duration-300 ease-out bg-black lg:py-6 group hover:bg-gradient-to-r hover:from-brandBlue hover:to-cyan-500">
+              <span className="absolute right-0 w-10 h-32 -mt-12 transition-all duration-1000 
         transform translate-x-12  bg-white opacity-5 rotate-12 group-hover:-translate-x-[500px] ease ">
-            </span>
-            Giving smartness to your business accounting 
-          </button>
-          <button className="relative flex-1 px-3 py-2 overflow-hidden text-white uppercase transition-all duration-300 ease-out bg-black border-l border-white lg:py-6 hover:border-l-0 group hover:bg-gradient-to-r hover:from-brandBlue hover:to-cyan-500 ">
-            <span className="absolute right-0 w-10 h-32 -mt-12 transition-all duration-1000 
+              </span>
+              Giving smartness to your business accounting
+            </button>
+            <button className="relative flex-1 px-3 py-2 overflow-hidden text-white uppercase transition-all duration-300 ease-out bg-black border-l border-white lg:py-6 hover:border-l-0 group hover:bg-gradient-to-r hover:from-brandBlue hover:to-cyan-500 ">
+              <span className="absolute right-0 w-10 h-32 -mt-12 transition-all duration-1000 
         transform translate-x-12 bg-white opacity-5 rotate-12 group-hover:-translate-x-[500px] ease">
-            </span>
-            Best Accounting Companies
-          </button>
-          <button className="relative flex-1 px-3 py-2 overflow-hidden text-white uppercase transition-all duration-300 ease-out bg-black border-l border-white lg:py-6 hover:border-l-0 group hover:bg-gradient-to-r hover:from-brandBlue hover:to-cyan-500 ">
-            <span className="absolute right-0 w-10 h-32 -mt-12 transition-all duration-1000 
+              </span>
+              Best Accounting Companies
+            </button>
+            <button className="relative flex-1 px-3 py-2 overflow-hidden text-white uppercase transition-all duration-300 ease-out bg-black border-l border-white lg:py-6 hover:border-l-0 group hover:bg-gradient-to-r hover:from-brandBlue hover:to-cyan-500 ">
+              <span className="absolute right-0 w-10 h-32 -mt-12 transition-all duration-1000 
         transform translate-x-12 bg-white opacity-5 rotate-12 group-hover:-translate-x-[500px] ease">
-            </span>
-            Corporate Tax Services
-          </button>
-        </div>
+              </span>
+              Corporate Tax Services
+            </button>
+          </div>
 
         </div>
       </div>
